38156cfb8e731ae94de887b27fd19d56d7fc80ba
Waleed Ahmed <waleed.ahmed@toptal.com>
2022-11-17 19:16:58 +0500

    Test Commit Thu 17 Nov 2022 07:16:58 PM PKT

--- go/main.go
+++ go/main.go
@@ -9,4 +9,6 @@ func main(){
 	fmt.Println("path:", os.Getenv("INPUT_PATH"))
 	fmt.Println("key:", os.Getenv("INPUT_KEY"))
 	fmt.Println("key:", os.Getenv("INPUT_RESTORE-KEYS"))
+	
+	fmt.Printf(`::set-output name=cache-hit::1`)
 }
